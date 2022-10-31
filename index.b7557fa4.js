e9446528a91fb8f6172d3ba2793337e56a121ffb
VishPoly <112825618+VishPoly@users.noreply.github.com>
2022-10-31 14:45:32 +0000

    Deploying to gh-pages from @ VishPoly/alliance@03025593ee7dd40ada52562f35eacfef9be2d4ee 🚀

--- index.b7557fa4.js
+++ index.b7557fa4.js
@@ -1,2 +1,2 @@
 !function(){var e={openModalBtn:document.querySelector("[data-modal-open]"),closeModalBtn:document.querySelector("[data-modal-close]"),modal:document.querySelector("[data-modal]")};function o(){e.modal.classList.toggle("is-hidden")}e.openModalBtn.addEventListener("click",o),e.closeModalBtn.addEventListener("click",o)}();
-//# sourceMappingURL=index.9a34c473.js.map
+//# sourceMappingURL=index.b7557fa4.js.map
