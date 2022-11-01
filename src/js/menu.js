2c5ecc59a6faeb2830e18c7058e501d99c8208a0
Poly <112825618+VishPoly@users.noreply.github.com>
2022-11-01 15:50:52 -0700

    commit menu

--- src/js/menu.js
+++ src/js/menu.js
@@ -1,23 +1,15 @@
 (() => {
     const refs = {
-    openMenuBtn: document.querySelector('[menu__btn-js]'),
-    closeMenuBtn: document.querySelector('[data-menu-close-hero]'),
-    menu: document.querySelector('[data-menu]'),
-    // menuList: document.querySelector('[mob-menu__list]'),
+    openMenuBtn: document.querySelector("[data-menu-open]"),
+    closeMenuBtn: document.querySelector("[data-menu-close]"),
+    menu: document.querySelector("[data-menu]"),
     };
-    console.log(refs.openMenuBtn)
-    refs.openMenuBtn.addEventListener('click', toggleMenu);
-    refs.closeMenuBtn.addEventListener('click', toggleMenu);
-    // refs.menuList.addEventListener('click', removeMenu);
+
+    refs.openMenuBtn.addEventListener("click", toggleMenu);
+    refs.closeMenuBtn.addEventListener("click", toggleMenu);
 
     function toggleMenu() {
-    refs.menu.classList.toggle('is-hidden');
-    document.querySelector('body').classList.toggle('is-body-hidden');
-    }
-
-    function removeMenu() {
-    refs.menu.classList.add('is-hidden');
-        document.querySelector('body').classList.remove('no-scroll');
+        refs.menu.classList.toggle("is-hidden");
         document.querySelector('body').classList.toggle('is-body-hidden');
     }
 })();
